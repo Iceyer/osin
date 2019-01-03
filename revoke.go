fb697879d751f0342734cf74f494fed34c038019
Iceyer <me@iceyer.net>
2019-01-03 10:08:44 +0800

    feat: implementation rfc7009

--- revoke.go
+++ revoke.go
@@ -4,44 +4,75 @@ import (
 	"net/http"
 )
 
+const (
+	E_UNSUPPORTED_TOKEN_TYPE = "unsupported_token_type"
+)
+
+// RevokeRequest store data for revoke
 type RevokeRequest struct {
-	AccessToken string
-	AccessData  *AccessData
+	Token         string `json:"token"`
+	TokenTypeHint string `json:"token_type_hint"`
+	AccessData    *AccessData
 }
 
+// HandleRevokeRequest implementation https://tools.ietf.org/html/rfc7009
 func (s *Server) HandleRevokeRequest(w *Response, r *http.Request) *RevokeRequest {
 	r.ParseForm()
 
-	// generate info request
-	ret := &RevokeRequest{
-		AccessToken: r.Form.Get("access_token"),
-	}
-
-	if ret.AccessToken == "" {
-		w.SetError(E_INVALID_REQUEST, "")
-		return nil
-	}
-
 	var err error
+	// generate info request
+	tokenTypeHint := r.Form.Get("token_type_hint")
+	token := r.Form.Get("token")
+	ret := &RevokeRequest{
+		Token: token,
+		TokenTypeHint: tokenTypeHint,
+	}
 
-	// load access data
-	ret.AccessData, err = w.Storage.LoadAccess(ret.AccessToken)
-	if err != nil {
+	if token == "" {
 		w.SetError(E_INVALID_REQUEST, "")
-		w.InternalError = err
-		return nil
-	}
-	if ret.AccessData == nil {
-		w.SetError(E_INVALID_REQUEST, "")
-		return nil
-	}
-	err = w.Storage.RemoveAccess(ret.AccessToken)
-	if err != nil {
-		w.SetError(E_SERVER_ERROR, "")
-		w.InternalError = err
 		return nil
 	}
 
+	switch tokenTypeHint {
+	case "access_token":
+		// load access data
+		ret.AccessData, err = w.Storage.LoadAccess(token)
+		if err != nil {
+			w.SetError(E_INVALID_REQUEST, "")
+			w.InternalError = err
+			return nil
+		}
+		if ret.AccessData == nil {
+			w.SetError(E_INVALID_REQUEST, "")
+			return nil
+		}
+		err = w.Storage.RemoveAccess(token)
+		if err != nil {
+			w.SetError(E_SERVER_ERROR, "")
+			w.InternalError = err
+			return nil
+		}
+		return ret
+	case "refresh_token":
+		ret.AccessData, err = w.Storage.LoadRefresh(token)
+		if err != nil {
+			w.SetError(E_INVALID_REQUEST, "")
+			w.InternalError = err
+			return nil
+		}
+		if ret.AccessData == nil {
+			w.SetError(E_INVALID_REQUEST, "")
+			return nil
+		}
+		err = w.Storage.RemoveRefresh(token)
+		if err != nil {
+			w.SetError(E_SERVER_ERROR, "")
+			w.InternalError = err
+			return nil
+		}
+		return ret
+	}
+	w.SetError(E_UNSUPPORTED_TOKEN_TYPE, "")
 	return ret
 }
 
